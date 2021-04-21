a8e50de2807d21dc78b3647d7b45bfec5737956a
Gold_Handy <omm_nc@hotmail.com>
2021-04-21 10:16:14 +0700

    change code format

--- qrcode_scanner.js
+++ qrcode_scanner.js
@@ -1,7 +1,8 @@
-function startScanning() {
+const codeReader = new ZXing.BrowserMultiFormatReader();
+
+function showScanningDialog() {
 
     let selectedDeviceId;
-    const codeReader = new ZXing.BrowserMultiFormatReader()
     console.log('ZXing code reader initialized')
     codeReader.listVideoInputDevices().then(
             function(videoInputDevices) {
@@ -18,33 +19,19 @@ function startScanning() {
 
                     sourceSelect.onchange = () => {
                         selectedDeviceId = sourceSelect.value;
+                        startScanning();
+                        codeReader.reset();
                     };
-
+                    startScanning();
                     const sourceSelectPanel = document.getElementById('sourceSelectPanel')
                     sourceSelectPanel.style.display = 'block'
                 }
 
-                document.getElementById("video").style.display = 'block';
-                codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-                    if (result) {
-                        console.log(result);
-                        //document.getElementById('result').textContent = result.text;
-                        document.getElementById('result').value = result.text;
-                        codeReader.reset();
-                        sourceSelectPanel.style.display = 'none';
-                        var audio = new Audio('resource/barcode-read.mp3');
-                        audio.play();
 
-                    }
-                    if (err && !(err instanceof ZXing.NotFoundException)) {
-                        console.error(err)
-                        document.getElementById('result').textContent = err
-                    }
-                })
                 console.log(`Started continous decode from camera with id ${selectedDeviceId}`);
 
                 document.getElementById('resetButton').addEventListener('click', () => {
-                    codeReader.reset()
+                    codeReader.reset();
                     document.getElementById('result').textContent = '';
                     sourceSelectPanel.style.display = 'none';
                     console.log('Reset.')
@@ -54,6 +41,31 @@ function startScanning() {
             function(err) {
                 console.error(err)
             });
+
+}
+
+function startScanning() {
+    let sourceSelect = document.getElementById('sourceSelect');
+    document.getElementById('sourceSelectPanel').style.display = 'block';
+    document.getElementById("video").style.display = 'block';
+    selectedDeviceId = sourceSelect.value;
+    console.log(selectedDeviceId);
+    codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+        if (result) {
+            console.log(result);
+            //document.getElementById('result').textContent = result.text;
+            document.getElementById('result').value = result.text;
+            codeReader.reset();
+            sourceSelectPanel.style.display = 'none';
+            var audio = new Audio('resource/barcode-read.mp3');
+            audio.play();
+
+        }
+        if (err && !(err instanceof ZXing.NotFoundException)) {
+            console.error(err)
+            document.getElementById('result').textContent = err
+        }
+    })
 
 }
 
